ea6356746c225de71898d6dbeafc2266a9ad0dde
pramodkumarp6 <pramodkumarp6@gmail.com>
2022-03-28 20:27:32 +0530

    wel python

--- app/src/main/java/com/pramod/consult/python/MainActivity.java
+++ app/src/main/java/com/pramod/consult/python/MainActivity.java
@@ -3,26 +3,34 @@ package com.pramod.consult.python;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.chaquo.python.PyObject;
 import com.chaquo.python.Python;
 import com.chaquo.python.android.AndroidPlatform;
 
-public class MainActivity extends AppCompatActivity {
+import org.w3c.dom.Text;
 
+public class MainActivity extends AppCompatActivity {
+private TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+             textView =  findViewById(R.id.text1);
 
         if(!Python.isStarted()) {
             Python.start(new AndroidPlatform(this));
         }
         Python python = Python.getInstance();
-        PyObject pyObject = python.getModule("");
+        PyObject pyObject = python.getModule("myscript");
 
-        PyObject object = pyObject.call("main");
+        PyObject object = pyObject.callAttr("main");
+
+        textView.setText(object.toString());
+
+
+
 
 
     }
---
+++ app/src/main/python/myscript.py
@@ -0,0 +1,2 @@
+def main():
+    return "welcome to india I m Love this Country"
