7f7186b6b208d645b9577414b2c10e8e0340b7d9
pramodkumarp6 <pramodkumarp6@gmail.com>
2022-03-28 19:47:24 +0530

    Initial commit

---
+++ app/src/main/java/com/pramod/consult/python/MainActivity.java
@@ -0,0 +1,29 @@
+package com.pramod.consult.python;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.chaquo.python.PyObject;
+import com.chaquo.python.Python;
+import com.chaquo.python.android.AndroidPlatform;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        if(!Python.isStarted()) {
+            Python.start(new AndroidPlatform(this));
+        }
+        Python python = Python.getInstance();
+        PyObject pyObject = python.getModule("");
+
+        PyObject object = pyObject.call("main");
+
+
+    }
+}
